1829026d190bde68051ca558809a750e60183dd2
raimu <koba54051@gmail.com>
2025-01-06 00:26:18 +0900

    add a command

--- mypkg/sensor_simulator.py
+++ mypkg/sensor_simulator.py
@@ -5,31 +5,28 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
 import math
-import time
 
 
-class NoisySensorSimulator(Node):
+class SensorSimulator(Node):
     def __init__(self):
-        super().__init__("noisy_sensor_simulator")
-        self.publisher = self.create_publisher(Float32, "noisy_sensor_data", 10)
-        self.start_time = time.time()
+        super().__init__("sensor_simulator")
+        self.publisher = self.create_publisher(Float32, "Noisy_sensor_data", 10)
+        self.step = 0
         self.create_timer(1.0, self.publish_sensor_data)
 
 
     def publish_sensor_data(self):
-        current_time = time.time() - self.start_time
-        base_value = 5.0 + 2.0 * math.sin(current_time)
-        noise = math.sin(current_time * 5) * 0.2
-        sensor_value = base_value + noise
+        self.step += 1
+        base_value = 5.0 + 2.0 * math.sin(self.step * 0.5)
 
         msg = Float32()
-        msg.data = sensor_value
+        msg.data = base_value
         self.publisher.publish(msg)
 
 
 def main():
     rclpy.init()
-    node = NoisySensorSimulator()
+    node = SensorSimulator()
     try:
         rclpy.spin(node)
     except rclpy.executors.ExternalShutdownException:
