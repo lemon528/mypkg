487a3733fe857dd4eab2e535537305f3dd9b5f7f
raimu <koba54051@gmail.com>
2025-01-06 00:41:51 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -7,13 +7,15 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
+source /opt/ros/humble/setup.bash
 source install/setup.bash
+
 
 log_file="/tmp/sensor_simulator.log"
 > $log_file
 
 ros2 run mypkg sensor_simulator & NODE_PID=$!
-ros2 topic echo /Noisy_sensor_data std_msgs/msg/Float32 >> $log_file & TOPIC_PID=$!
+ros2 topic echo /sensor_data std_msgs/msg/Float32 >> $log_file & TOPIC_PID=$!
 
 sleep 30
 
@@ -25,7 +27,10 @@ grep -q -E "data: 3\.[0-9]*" $log_file && echo "最小値(約3.0)を確認" || {
 
 grep -q -E "data: 6\.9[0-9]*" $log_file && echo "最大値(約7.0)を確認" || { echo "最大値が確認できません"; res=1; }
 
-kill $NODE_PID $TOPIC_PID
+ps -p $NODE_PID $TOPIC_PID > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    kill $NODE_PID $TOPIC_PID
+fi
 wait $NODE_PID $TOPIC_PID 2>/dev/null
 
 if [ "$res" -eq 0 ]; then
